e5aa826a50ddab4c75d18300681058c47560b62e
Mathieu CHARLES <87909957+mathieu0c@users.noreply.github.com>
2024-05-01 01:57:58 +0200

    If minimal version is not met, download the installer directly (signed)

--- libSimpleUpdater/UpdateHandler.hpp
+++ libSimpleUpdater/UpdateHandler.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
+#include <QApplication>
+#include <QCloseEvent>
 #include <QDialog>
 #include <QMessageBox>
-
-#include <QCloseEvent>
-
 #include <SimpleUpdater.hpp>
 
 namespace updt {
@@ -13,106 +12,111 @@ namespace Ui {
 class UpdateHandler;
 }
 
-class UpdateHandler : public QWidget
-{
-    Q_OBJECT
+class UpdateHandler : public QWidget {
+  Q_OBJECT
 
-public:
-    struct States{
-        enum State{
-            kReset,
-            kRetrievingReleaseInfo,
-            kReleaseInfoRetrieved,
-            kRetrievingManifest,
-            kManifestRetrieved,
-            kDownloadingUpdatePackage,
-            kUpdatePackageRetrieved,
-        };
+ public:
+  struct States {
+    enum State {
+      kReset,
+      kRetrievingReleaseInfo,
+      kReleaseInfoRetrieved,
+      kRetrievingManifest,
+      kManifestRetrieved,
+      kDownloadingUpdatePackage,
+      kUpdatePackageRetrieved,
     };
+  };
 
-    struct InfoBoxMsgType{
-        enum Type{
-            kUnknown,
-            kWarning,
-            kOk,
-            kCritical
-        };
+  struct InfoBoxMsgType {
+    enum Type { kUnknown, kWarning, kOk, kCritical };
 
-        static constexpr QColor kUnknownColor{255,255,255,255};
-        static constexpr QColor kUnknownTextColor{243,166,0};
-        static const QString kUnknownTitleText;
+    static constexpr QColor kUnknownColor{255, 255, 255, 255};
+    static constexpr QColor kUnknownTextColor{243, 166, 0};
+    static const QString kUnknownTitleText;
 
-        static constexpr QColor kWarningColor{250,243,216};
-        static constexpr QColor kWarningTextColor{243,166,0};
-        static const QString kWarningTitleText;
+    static constexpr QColor kWarningColor{250, 243, 216};
+    static constexpr QColor kWarningTextColor{243, 166, 0};
+    static const QString kWarningTitleText;
 
-        static constexpr QColor kOkColor{216,241,227};
-        static constexpr QColor kOkTextColor{68,184,111};
-        static const QString kOkTitleText;
+    static constexpr QColor kOkColor{216, 241, 227};
+    static constexpr QColor kOkTextColor{68, 184, 111};
+    static const QString kOkTitleText;
 
-        static constexpr QColor kCriticalColor{248,209,204};
-        static constexpr QColor kCriticalTextColor{182,24,37};
-        static const QString kCriticalTitleText;
-    };
+    static constexpr QColor kCriticalColor{248, 209, 204};
+    static constexpr QColor kCriticalTextColor{182, 24, 37};
+    static const QString kCriticalTitleText;
+  };
 
-public:
-    explicit UpdateHandler(Version progRunningVersion,QString githubReleaseApiAddress,
-                           QString publicVerifierKeyFile, bool searchAvailableOnCreation,
-                           QString postUpdateCmd,
-                           bool showInstallPropositionOnNextOccasion, QWidget *parent);
-    ~UpdateHandler();
+ public:
+  explicit UpdateHandler(Version progRunningVersion, QString githubReleaseApiAddress, QString publicVerifierKeyFile,
+                         bool searchAvailableOnCreation, QString postUpdateCmd,
+                         bool showInstallPropositionOnNextOccasion, QWidget* parent);
+  ~UpdateHandler();
 
-    void showInstallPropositionOnNextOccasion(){m_showInstallPropositionOnNextOccasion = true;}
+  void showInstallPropositionOnNextOccasion() {
+    m_showInstallPropositionOnNextOccasion = true;
+  }
 
-    void showInfoMessage(InfoBoxMsgType::Type type,const QString& message,int32_t timeout = 0);
-    void hideInfoMessage();
+  void showInfoMessage(InfoBoxMsgType::Type type, const QString& message, int32_t timeout = 0);
+  void hideInfoMessage();
 
-public:
-    static std::optional<qint64> startDetachedUpdateProcess(const QString& packagePath,
-                                                            const QString& verifierKeyPath,
-                                                            const QString& updateManifest,
-                                                            const QString& postUpdateCmd);
+ public:
+  static std::optional<qint64> startDetachedUpdateProcess(const QString& packagePath, const QString& verifierKeyPath,
+                                                          const QString& updateManifest, const QString& postUpdateCmd);
+  static std::optional<qint64> startProcess(const QString& executable, const QStringList& argsList = {},
+                                            const QString& working_dir = QApplication::applicationDirPath());
 
-private slots:
-    void on_pb_close_clicked();
-    void on_pb_checkAvailable_clicked();
-    void on_pb_downloadAndInstall_clicked();
+ private slots:
+  void on_pb_close_clicked();
+  void on_pb_checkAvailable_clicked();
+  void on_pb_downloadAndInstall_clicked();
 
-    void on_pb_manualInstall_clicked();
+  void on_pb_manualInstall_clicked();
 
-private:
-    void resetState(const QString& errMsg = {});//use in case an error occurs. Reset everything
-    void setReadyToUpdate(bool state = true){if(!state)resetState();else m_readyToUpdate = true;}
+ private:
+  void resetState(const QString& errMsg = {});  // use in case an error occurs. Reset everything
+  void setReadyToUpdate(bool state = true) {
+    if (!state)
+      resetState();
+    else
+      m_readyToUpdate = true;
+  }
 
-    void setState(States::State newState,bool keepInfoBoxVisible = true);
+  void setState(States::State newState, bool keepInfoBoxVisible = true);
 
-    void downloadAndInstall(bool onlyRetrieveManifest);
-    void checkAvailableOnline(bool installAfterward);
-    void onLatestUpdateRetrieved(std::optional<updt::ReleaseInfo> releaseInfoOpt, bool installAfterwards);
-    void onManifestRetrieved(std::optional<QJsonDocument> docOpt, bool install);
-    void onUpdatePackageRetrieved();
+  void downloadAndInstall(bool onlyRetrieveManifest);
+  void checkAvailableOnline(bool installAfterward);
+  void onLatestUpdateRetrieved(std::optional<updt::ReleaseInfo> releaseInfoOpt, bool installAfterwards);
+  void onManifestRetrieved(std::optional<QJsonDocument> docOpt, bool install);
+  void onUpdatePackageRetrieved();
+  void onInstallerRetrieved(const QString& installer_path);
 
-private:
-    Ui::UpdateHandler *ui;
+  void updateFromInstaller(const updt::Manifest& manifest);
+  void updateFromPackage();
 
-    States::State m_state{States::kReset};
-    bool m_askedToReset{false};
+ private:
+  Ui::UpdateHandler* ui;
 
-    const Version m_kRunningVersion;
-    const QString m_kGithubReleaseApiAddress;
-    const QString m_kPblicVerifierKeyFile;
-    const QString m_kPostUpdateCmd;
-    const QString m_kUpdatePackageName{"Update.pck"};
-    const QString m_kDistantManifestName{"UpdateManifest.json"};
-    const QString m_kDownloadDir{"tmpDownloads/"};
+  States::State m_state{States::kReset};
+  bool m_askedToReset{false};
 
-    bool m_readyToUpdate{false};
-    std::optional<ReleaseInfo> m_latestReleaseInfoOpt{};
-    bool m_showInstallPropositionOnNextOccasion;/*!< Propose the user to install the updated  */
+  const Version m_kRunningVersion;
+  const QString m_kGithubReleaseApiAddress;
+  const QString m_kPblicVerifierKeyFile;
+  const QString m_kPostUpdateCmd;
+  const QString m_kUpdatePackageName{"Update.pck"};
+  const QRegularExpression m_kInstallerRegex{R"(setup.*x64\.exe)"};
+  const QString m_kDistantManifestName{"UpdateManifest.json"};
+  const QString m_kDownloadDir{"tmpDownloads/"};
 
-    QString m_baseInfoMsg{};
-    QString m_prevInfoMsgHash{};
-    int32_t m_infoMsgRepetitionCount{};
+  bool m_readyToUpdate{false};
+  std::optional<ReleaseInfo> m_latestReleaseInfoOpt{};
+  bool m_showInstallPropositionOnNextOccasion; /*!< Propose the user to install the updated  */
+
+  QString m_baseInfoMsg{};
+  QString m_prevInfoMsgHash{};
+  int32_t m_infoMsgRepetitionCount{};
 };
 
-} // namespace updt
+}  // namespace updt
